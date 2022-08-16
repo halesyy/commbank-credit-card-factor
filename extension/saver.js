864ddbc61a3f0f47385c0609ad609de041e07e96
Jack Hales <halesyy@gmail.com>
2022-08-16 22:24:36 +1000

    Full lifecycle now working

--- extension/saver.js
+++ extension/saver.js
@@ -14,10 +14,16 @@ console.log("If you find any issues or have feature requests, feel free to get i
 // Iterates through all visible accounts, returning their values.
 let getAccounts = () => {
   let accounts = [...document.querySelectorAll("div.account-wrapper")]
-  let available = accounts.map(account => [
-    account.querySelector("h3.account-name").innerHTML.trim(),
-    parseFloat(account.querySelector("span.monetary-value").innerHTML.trim().replace("$","").replace(/,/g, ""))
-  ])
+  let available = []
+  for (let account of accounts) {
+    try {
+      let name = account.querySelector("h3.account-name").innerHTML.trim()
+      let value = parseFloat(account.querySelector("ul.account-balances li.is-bold span.monetary-value").getAttribute("title").trim().replace("$","").replace(/,/g, ""))
+      available.push([name, value])
+    } catch {
+      // console.log("Parsing an account failed!", account)
+    }
+  }
   return available
 }
 
@@ -53,6 +59,31 @@ let checkForCreditLimit = () => {
   }
 }
 
+// Given the local storage data + current available data, will
+// attempt to calculate the offset.
+let calculateCreditOffset = async available => {
+  return new Promise((resolve, reject) => {
+    let creditOffset = 0
+    // Fetch the credit limits.
+    chrome.storage.local.get({accountLimits: {}}, ({accountLimits}) => {
+      for (let [accountName, availableValue] of available) {
+        if (accountName in accountLimits) {
+          // Calculate the limit (max), the available amount (after spending) and spend.
+          let creditLimit = accountLimits[accountName]
+          let creditAvailable = availableValue
+          let creditSpent = creditLimit - creditAvailable
+          // console.log(creditLimit, creditAvailable, creditSpent)
+          // Make sure only adding numbers.
+          if (!isNaN(creditSpent)) {
+            creditOffset += creditSpent
+          }
+        }
+      }
+      resolve(creditOffset)
+    })
+  })
+}
+
 // Go over the given accounts, and if there's a credit
 // which can be calculated out, then use it and apply it
 // to the smart access value.
@@ -61,17 +92,29 @@ let overlayFindings = () => {
   let available = getAccounts() // [accountName, available]
   let accountNames = available.map(account => account[0])
   let accountIndex = accountNames.indexOf(spendingAccount)
-  let everydayAvailable = [...document.querySelectorAll("div.account-wrapper")][accountIndex]
+  let everydayAccountDom = [...document.querySelectorAll("div.account-wrapper")][accountIndex]
   let everydayValue = available[accountIndex][1]
+  // console.log(everydayValue)
 
   // Calculate credit offset for avail.
-  console.log(everydayValue)
+  calculateCreditOffset(available).then(offset => {
+    let everydayAfter = everydayValue - offset
+    everydayAfter = everydayAfter.toFixed(2) // parse to float
+    everydayAfter = `$${everydayAfter}`
+    // console.log("Everyday after is", everydayAfter)
+
+    // Find the available amount and change it to view.
+    let priceDom = everydayAccountDom.querySelector("ul.account-balances li.is-bold span.monetary-value")
+    priceDom.innerHTML = everydayAfter
+    // priceDom.style = "color: gray;"
+  })
 
 }
-overlayFindings()
 
-
-// let accounts = [...document.querySelectorAll("div.account-wrapper")]
-setTimeout(checkForCreditLimit, 1000)
+setTimeout(checkForCreditLimit, 500)
 setInterval(checkForCreditLimit, 10000) // Check the users page every 10s for a credit limit.
+
+setTimeout(overlayFindings, 100)
+setTimeout(overlayFindings, 200)
+setTimeout(overlayFindings, 500)
 setInterval(overlayFindings, 2500)
